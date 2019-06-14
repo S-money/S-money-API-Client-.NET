4bd740eb8b1986a745a0de7a581784ce5e7df70f
vincentlegal <vincent@bolden.fr>
2019-06-14 10:04:44 -0700

    #889 : implement GetHistoryItems

--- S-money.API.Client.Public/PaymentsClient.cs
+++ S-money.API.Client.Public/PaymentsClient.cs
@@ -38,5 +38,21 @@ namespace Smoney.API.Client
             var uri = CreateUri(userId, payments);
             return PostAsync(uri, payment);
         }
+
+        public IEnumerable<HistoryItem> GetHistoryItems(string userId = null, DateTime? startDate = null, DateTime? endDate = null, int perPage = 10000, int page = 1)
+        {
+            var path = string.Format("{0}{1}{2}{3}", "historyitems?perpage=", perPage, "&page=", page);
+            if (startDate.HasValue)
+            {
+                path = string.Format("{0}{1}{2}", path, "&DateStart=", startDate.Value.ToString("O"));
+            }
+            if (endDate.HasValue)
+            {
+                path = string.Format("{0}{1}{2}", path, "&DateEnd=", endDate.Value.ToString("O"));
+            }
+
+            var uri = CreateUri(userId, path);
+            return GetAsync<IEnumerable<HistoryItem>>(uri);
+        }
     }
 }
